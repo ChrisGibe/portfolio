6d5a1114f9865d3c2b2c9ffa34f6c6d88f1046b2
Christophe Gibelli <cgibelli@tequilarapido.com>
2026-08-13 08:03:04 +0200

    👷 Test(CI/CD) : Checking CI/CD

--- web/app/themes/cgibelli/template-parts/header/nav.php
+++ web/app/themes/cgibelli/template-parts/header/nav.php
@@ -1,6 +1,8 @@
 <nav class="flex place-content-center w-full">
     <a href="<?= esc_url(home_url()); ?>" aria-label="Back to home">
-        <span class="text-red-500">TEST</span>
+        <span class="text-red-500">
+            TEST
+        </span>
         <span class="neue text-2xl font-bold flex items-baseline justify-center uppercase home-link">
             gibelli
             <span class="inline-flex circle h-1 w-1 bg-black rounded-full"></span>
